491c6ea7b4da7657799fbf4fac28715666c70115
Matt Hough <matt@hough.tv>
2017-09-09 23:12:39 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -102,9 +102,7 @@ end
 
 #returns true if the board has not been won but is full, false if the board is not won and the board is not full, and false if the board is won
 def draw?(board)
-  if full?(board)
-    return true
-  else
+  if won?(board)
     false
   end
 end
