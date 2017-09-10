256025e7b1041e7ecdd7a93a74c69becb2f968c5
Matt Hough <matt@hough.tv>
2017-09-10 02:06:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -122,9 +122,9 @@ end
 
 #returns the current player (X or O) depending on who won
 def winner(board)
-  if won?(board)[0] == "X"
+  if won?(board) && won?(board)[0] == "X"
     return "X"
-  elsif won?(board)[0] == "O"
+  elsif won?(board) && won?(board)[0] == "O"
     return "O"
   else
     puts "false"
